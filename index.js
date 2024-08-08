603d3b4477d412a51591d131852cd763d9057402
ejaramillom <ejaramilo@hotmail.com>
2024-08-08 19:14:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// what is nodejs
+
+/*
+it is a runtime environment for javascript so you can run js code outside the browser (v8 is the engine that runs js in the browser)
+
+an engine takes js code, compiles it to machine code, and then transforms it in actions in the server (computer) and adds js features (it was written in C++) so you can use files and other features just by running js code
+*/
+
